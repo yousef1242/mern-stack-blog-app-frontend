56e8acf76ed9dbc4fbeed666c1dd5cc07a0b674f
yousef <yousefahmedelshahed@gmail.com>
2023-05-14 21:48:13 +0300

    deploy

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const request = axios.create({
-    baseURL : "http://localhost:8080"
+    baseURL : "https://blog-app-mern-stack.onrender.com"
 })
 
 export default request
