c38007dbe6e518709c1b844f3ffb7b35204414ad
yousef <yousefahmedelshahed@gmail.com>
2023-05-17 12:50:35 +0300

    make loading

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,18 +1,21 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "../../redux/apiCalls/postApiCall";
 import PostList from "../../components/posts/PostList";
-import "./home.css";
 import Sidebar from "../../components/slide/SideBar";
 import { Link } from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux"
-import { fetchPosts } from "../../redux/apiCalls/postApiCall";
-import { useEffect } from "react";
-
+import "./home.css";
+import LoaderPosts from "../../components/loader-posts/loaderPosts";
 
 const HomePage = () => {
-  const {posts} = useSelector(state => state.posts)
-  const dispatch = useDispatch()
+  const [loading, setLoading] = useState(true);
+  const { posts } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(fetchPosts(1))
-  },[])
+    dispatch(fetchPosts(1)).then(() => setLoading(false));
+  }, [dispatch]);
+
   return (
     <>
       <section className="home">
@@ -22,14 +25,20 @@ const HomePage = () => {
           </div>
         </div>
         <div className="home-latest-post">Latest Posts</div>
-        <div className="home-container">
-          <PostList posts={posts} />
-          <Sidebar />
+        <div className={loading ? "d-block" : "home-container"}>
+          {loading ? (
+            <LoaderPosts/>
+          ) : (
+            <>
+              <PostList posts={posts} />
+              <Sidebar />
+            </>
+          )}
         </div>
         <div className="home-see-post-link">
-            <Link to={"/posts"} className="home-link">
-                See All Posts
-            </Link>
+          <Link to={"/posts"} className="home-link">
+            See All Posts
+          </Link>
         </div>
       </section>
     </>
--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -5,34 +5,44 @@ import Sidebar from "../../components/slide/SideBar";
 import "./postPage.css";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPosts, fetchPostsCount } from "../../redux/apiCalls/postApiCall";
+import LoaderPosts from "../../components/loader-posts/loaderPosts";
 
 const POST_PER_PAGE = 3;
 
 const PostPage = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const { postCount, posts } = useSelector((state) => state.posts);
   const pages = Math.ceil(postCount / POST_PER_PAGE);
 
   useEffect(() => {
-      dispatch(fetchPosts(currentPage));
-      window.scrollTo(0, 0);
-    }, [currentPage]);
-    
-    useEffect(() => {
-      dispatch(fetchPostsCount());
-    }, []);
+    dispatch(fetchPosts(currentPage)).then(() => {
+      setLoading(false);
+    });
+    window.scrollTo(0, 0);
+  }, [currentPage]);
+
+  useEffect(() => {
+    dispatch(fetchPostsCount()).then(() => setLoading(false));
+  }, []);
   return (
     <>
-      <section className="post-page">
-        <PostList posts={posts} />
-        <Sidebar/>
-      </section>
-      <Pagination
-        pages={pages}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
+      {loading ? (
+        <LoaderPosts load={"loasd"} />
+      ) : (
+        <>
+          <section className="post-page">
+            <PostList posts={posts} />
+            <Sidebar />
+          </section>
+          <Pagination
+            pages={pages}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+          />
+        </>
+      )}
     </>
   );
 };
